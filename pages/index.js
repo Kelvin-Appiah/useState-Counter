955baf682bc32d7f268b1eda5119fe88ba140d02
Kelvin-Squishy <obengappiah98@gmail.com>
2022-09-18 16:41:08 +0800

    styling will be enhanced

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,48 @@ import styles from '../styles/Home.module.css';
 import { useState } from 'react';
 
 export default function Home() {
-  return <h1>Hey</h1>;
+  const [number, setNumber] = useState(0);
+
+  const Subtract = () => {
+    setNumber(number - 1);
+  };
+
+  const Add = () => {
+    setTimeout(() => {
+      setNumber((prevNum) => {
+        return prevNum + 1;
+      });
+    }, 2000);
+  };
+
+  return (
+    <section className={styles.main}>
+      <h1 style={{ fontSize: '2.5rem' }}>useState Counter</h1>
+      <div className={styles.number}>
+        <span>{number}</span>
+      </div>
+      <div className={styles.Thebuttons}>
+        <button
+          className={`${styles.Abutton} ${styles.redButton}`}
+          onClick={Subtract}
+        >
+          - Subtract
+        </button>
+        <button
+          className={`${styles.Abutton} ${styles.whiteButton}`}
+          onClick={() => {
+            setNumber(0);
+          }}
+        >
+          Reset
+        </button>
+        <button
+          className={`${styles.Abutton} ${styles.blueButton}`}
+          onClick={Add}
+        >
+          + Add
+        </button>
+      </div>
+    </section>
+  );
 }
